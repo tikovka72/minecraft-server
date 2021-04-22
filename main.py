73cc303cf76ec9ce225f1e244d305523d60b9b89
Tikovka72 <zeytunyan2004@mail.ru>
2021-04-22 18:36:31 +0700

    add method for command identification by id

--- main.py
+++ main.py
@@ -24,15 +24,26 @@ class Listener:
     def start(self):
         self.run_commands()
 
-    def get_command(self, command: str):
-        return "/give Mihendy gold_block"  # test
-        # return requests.get(self.end_point + "/command_id", params={"key": self.key, "command": command}).json()
+    @staticmethod
+    def replace_user(command, user):
+        if "<user>" in command:
+            return command.replace("<user>", user)
+        return command
+
+    def get_command(self, command_id: str):
+        # return "/give Mihendy gold_block"  # test
+        r = requests.get(self.end_point + "/get_command",
+                         params={"key": self.key, "command_id": command_id})
+        if r.status_code != 200:
+            return None
+        return r.json()["command"]
 
     def run_commands(self):
-        for command_id in self:
+        for pay_id, command_id, username in self:
             command = self.get_command(command_id)
+            command_with_user = self.replace_user(command, username)
             if self.server:
-                self.server.stdin.write(bytes(command + "\r\n", "ascii"))
+                self.server.stdin.write(bytes(command_with_user-+ + "\r\n", "ascii"))
                 self.server.stdin.flush()
 
     def __iter__(self):
@@ -42,11 +53,11 @@ class Listener:
         resp = 0
         code = NOT_MOD
         while code == NOT_MOD:
-            resp = requests.get(self.end_point + "/mock_resolve", params={"key": self.key})
+            resp = requests.get(self.end_point + "/resolve", params={"key": self.key})
             code = resp.status_code
         if code != SUCCESS:
             return
-        return resp.json()[1]
+        return resp.json()
 
 
 if __name__ == "__main__":
